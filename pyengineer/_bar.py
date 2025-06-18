5a903e4a46da74ab6aa924af6658ed7419826df8
lidaltonlima <lidaltonlima@gmail.com>
2025-06-18 18:03:27 -0400

    refactor: cálculo das matrizes da barra na classe bar

--- pyengineer/_bar.py
+++ pyengineer/_bar.py
@@ -1,5 +1,5 @@
 """Módulo para operações matemáticas"""
-from numpy import sqrt
+import numpy as np
 
 from ._material import Material
 from ._node import Node
@@ -12,7 +12,8 @@ class Bar:
                  start_node: Node,
                  end_node: Node,
                  section: Section,
-                 material: Material):
+                 material: Material,
+                 rotation: float = 0):
         """Construtor
 
         Args:
@@ -25,12 +26,126 @@ class Bar:
         self.name = name
         self.start_node = start_node
         self.end_node = end_node
+        self.section = section
+        self.material = material
+        self.rotation = rotation
         self.dx = end_node.x - start_node.x
         self.dy = end_node.y - start_node.y
         self.dz = end_node.z - start_node.z
-        self.length = sqrt(self.dx**2 + self.dy**2 + self.dz**2)
-        self.section = section
-        self.material = material
+        self.length = np.sqrt(self.dx**2 + self.dy**2 + self.dz**2)
         self.klg = None # Matriz de rigidez nas coordenadas globais
-        self.kl = None # Matriz de rigidez nas coordenadas locais
-        self.r = None # Matriz de rotação
+        self.kl = self.calculate_kl() # Matriz de rigidez nas coordenadas locais
+        self.r = self.calculate_r() # Matriz de rotação
+
+
+    def calculate_kl(self):
+        """Calcula a matriz de rigidez local da barra
+
+        Args:
+            bar (Bar): Barra
+
+        Returns:
+            ndarray: matriz de rigidez local
+        """
+        kl = np.zeros([12, 12])
+
+        l = self.length
+        a = self.section.area
+        ix = self.section.ix
+        iy = self.section.iy
+        iz = self.section.iz
+        e = self.material.e
+        g = self.material.g
+
+        kl[0][0] = (e * a) / l
+        kl[0][6] = -kl[0][0]
+        kl[1][1] = (12 * e * iz) / l**3
+        kl[1][5] = (6 * e * iz) / l**2
+        kl[1][7] = -kl[1][1]
+        kl[1][11] = kl[1][5]
+        kl[2][2] = (12 * e * iy) / l**3
+        kl[2][4] = (-6 * e * iy) / l**2
+        kl[2][8] = -kl[2][2]
+        kl[2][10] = kl[2][4]
+        kl[3][3] = (g * ix) / l
+        kl[3][9] = -kl[3][3]
+        kl[4][4] = (4 * e * iy) / l
+        kl[4][8] = -kl[2][4]
+        kl[4][10] = (2 * e * iy) / l
+        kl[5][5] = (4 * e * iz) / l
+        kl[5][7] = -kl[1][5]
+        kl[5][11] = (2 * e * iz) / l
+        kl[6][6] = kl[0][0]
+        kl[7][7] = kl[1][1]
+        kl[7][11] = -kl[1][5]
+        kl[8][8] = kl[2][2]
+        kl[8][10] = -kl[2][4]
+        kl[9][9] = kl[3][3]
+        kl[10][10] = kl[4][4]
+        kl[11][11] = kl[5][5]
+        kl = kl + kl.T - np.diag(kl.diagonal())
+
+        self.kl = kl # Atribui ao objeto
+
+        return kl
+
+    def calculate_r(self):
+        """Calcula a matriz de rotação da barra
+
+        Args:
+            bar (Bar): barra
+
+        Returns:
+            ndarray: matriz de rotação
+        """
+        x1 = self.end_node.x
+        y1 = self.end_node.y
+        z1 = self.end_node.z
+
+        dx = self.dx
+        dy = self.dy
+        dz = self.dz
+
+        rot_aux = np.zeros([3, 3])
+        rot_aux[0, 0] = dx / self.length
+        rot_aux[0, 1] = dy / self.length
+        rot_aux[0, 2] = dz / self.length
+
+        # Ponto auxiliar para determinar o plano "xy" da barra
+        if dx != 0 or dz != 0:
+            aux = np.array([x1, y1 + 1, z1])
+        else:
+            aux = np.array([x1 + 1, y1, z1])
+
+        dx = aux[0] - x1
+        dy = aux[1] - y1
+        dz = aux[2] - z1
+        c = np.sqrt(dx**2 + dy**2 + dz**2)
+
+        alpha = dx / c
+        beta = dy / c
+        gamma = dz / c
+
+        dx = rot_aux[0, 1] * gamma - rot_aux[0, 2] * beta
+        dy = rot_aux[0, 2] * alpha - rot_aux[0, 0] * gamma
+        dz = rot_aux[0, 0] * beta - rot_aux[0, 1] * alpha
+        c = np.sqrt(dx**2 + dy**2 + dz**2)
+
+        rot_aux[2, 0] = dx / c
+        rot_aux[2, 1] = dy / c
+        rot_aux[2, 2] = dz / c
+
+        rot_aux[1, 0] = rot_aux[0, 2] * rot_aux[2, 1] - rot_aux[0, 1] * rot_aux[2, 2]
+        rot_aux[1, 1] = rot_aux[0, 0] * rot_aux[2, 2] - rot_aux[0, 2] * rot_aux[2, 0]
+        rot_aux[1, 2] = rot_aux[0, 1] * rot_aux[2, 0] - rot_aux[0, 0] * rot_aux[2, 1]
+
+        rotation = np.zeros([12, 12])
+
+        rotation[0:3, 0:3] = rot_aux
+        rotation[3:6, 3:6] = rot_aux
+        rotation[6:9, 6:9] = rot_aux
+        rotation[9:12, 9:12] = rot_aux
+
+        self.r = rotation # Atribui matriz de rotação no objeto barra
+
+        return rotation
--- pyengineer/analysis/_linear.py
+++ pyengineer/analysis/_linear.py
@@ -93,58 +93,6 @@ class Linear:
         return kg
 
 
-    def calculate_kl(self, bar: Bar):
-        """Calcula a matriz de rigidez local da barra
-
-        Args:
-            bar (Bar): Barra
-
-        Returns:
-            ndarray: matriz de rigidez local
-        """
-        kl = np.zeros([12, 12])
-
-        l = bar.length
-        a = bar.section.area
-        ix = bar.section.ix
-        iy = bar.section.iy
-        iz = bar.section.iz
-        e = bar.material.e
-        g = bar.material.g
-
-        kl[0][0] = (e * a) / l
-        kl[0][6] = -kl[0][0]
-        kl[1][1] = (12 * e * iz) / l**3
-        kl[1][5] = (6 * e * iz) / l**2
-        kl[1][7] = -kl[1][1]
-        kl[1][11] = kl[1][5]
-        kl[2][2] = (12 * e * iy) / l**3
-        kl[2][4] = (-6 * e * iy) / l**2
-        kl[2][8] = -kl[2][2]
-        kl[2][10] = kl[2][4]
-        kl[3][3] = (g * ix) / l
-        kl[3][9] = -kl[3][3]
-        kl[4][4] = (4 * e * iy) / l
-        kl[4][8] = -kl[2][4]
-        kl[4][10] = (2 * e * iy) / l
-        kl[5][5] = (4 * e * iz) / l
-        kl[5][7] = -kl[1][5]
-        kl[5][11] = (2 * e * iz) / l
-        kl[6][6] = kl[0][0]
-        kl[7][7] = kl[1][1]
-        kl[7][11] = -kl[1][5]
-        kl[8][8] = kl[2][2]
-        kl[8][10] = -kl[2][4]
-        kl[9][9] = kl[3][3]
-        kl[10][10] = kl[4][4]
-        kl[11][11] = kl[5][5]
-        kl = kl + kl.T - np.diag(kl.diagonal())
-
-        bar.kl = kl # Atribui ao objeto
-
-        return kl
-
-
     def calculate_klg(self, bar: Bar):
         """Transforma a matriz de rigidez local e global
 
@@ -154,8 +102,8 @@ class Linear:
         Returns:
             _type_: _description_
         """
-        r = self.calculate_r(bar)
-        klg = r.T @ self.calculate_kl(bar) @ r
+        r = bar.r
+        klg = r.T @ bar.kl @ r
 
         bar.klg = klg # Atribui ao objeto
 
@@ -199,68 +147,6 @@ class Linear:
 
 
         return kg_solution
-
-
-    def calculate_r(self, bar: Bar):
-        """Calcula a matriz de rotação da barra
-
-        Args:
-            bar (Bar): barra
-
-        Returns:
-            ndarray: matriz de rotação
-        """
-        x1 = bar.end_node.x
-        y1 = bar.end_node.y
-        z1 = bar.end_node.z
-
-        dx = bar.dx
-        dy = bar.dy
-        dz = bar.dz
-
-        rot_aux = np.zeros([3, 3])
-        rot_aux[0, 0] = dx / bar.length
-        rot_aux[0, 1] = dy / bar.length
-        rot_aux[0, 2] = dz / bar.length
-
-        # Ponto auxiliar para determinar o plano "xy" da barra
-        if dx != 0 or dz != 0:
-            aux = np.array([x1, y1 + 1, z1])
-        else:
-            aux = np.array([x1 + 1, y1, z1])
-
-        dx = aux[0] - x1
-        dy = aux[1] - y1
-        dz = aux[2] - z1
-        c = np.sqrt(dx**2 + dy**2 + dz**2)
-
-        alpha = dx / c
-        beta = dy / c
-        gamma = dz / c
-
-        dx = rot_aux[0, 1] * gamma - rot_aux[0, 2] * beta
-        dy = rot_aux[0, 2] * alpha - rot_aux[0, 0] * gamma
-        dz = rot_aux[0, 0] * beta - rot_aux[0, 1] * alpha
-        c = np.sqrt(dx**2 + dy**2 + dz**2)
-
-        rot_aux[2, 0] = dx / c
-        rot_aux[2, 1] = dy / c
-        rot_aux[2, 2] = dz / c
-
-        rot_aux[1, 0] = rot_aux[0, 2] * rot_aux[2, 1] - rot_aux[0, 1] * rot_aux[2, 2]
-        rot_aux[1, 1] = rot_aux[0, 0] * rot_aux[2, 2] - rot_aux[0, 2] * rot_aux[2, 0]
-        rot_aux[1, 2] = rot_aux[0, 1] * rot_aux[2, 0] - rot_aux[0, 0] * rot_aux[2, 1]
-
-        rotation = np.zeros([12, 12])
-
-        rotation[0:3, 0:3] = rot_aux
-        rotation[3:6, 3:6] = rot_aux
-        rotation[6:9, 6:9] = rot_aux
-        rotation[9:12, 9:12] = rot_aux
-
-        bar.r = rotation # Atribui matriz de rotação à no objeto barra
-
-        return rotation
 
     def calculate_spread_vector(self, bar: Bar):
         """Calcula o vetor de espalhamento
