65ce1f06cae69632ec4c051aa39eac86edd659f3
lidaltonlima <lidaltonlima@gmail.com>
2025-06-19 11:44:19 -0400

    feat(_structure): scene initial

--- pyengineer/view/_structure.py
+++ pyengineer/view/_structure.py
@@ -1,41 +1,99 @@
 """Módulo para visualização da estrutura"""
+import typing as tp
 import pyvista as pv
 import vtk
 
-# pylint: disable=E1101
-vtk.vtkObject.GlobalWarningDisplayOff()
-
-# configurações de tema
-pv.set_plot_theme('dark')
-
-# Cena
-plotter = pv.Plotter()
-plotter.camera.parallel_projection = True # Projeção ortográfica
-plotter.background_color = '#1a1a1a' # Cor de fundo
-plotter.enable_terrain_style(mouse_wheel_zooms=True) # Orbit com o limite no topo
-plotter.camera.view_up = (0, 0, 1) # Eixo "z" para cima
-plotter.add_axes() # Visualização do eixos globais
-
-# Visualização
-plotter.camera_position = [
-    (100, -100, 100),  # posição da câmera (x, y, z)
-    (0, 0, 0),     # ponto focal (olhando pro centro da cena)
-    (0, 0, 1)      # view-up (o eixo Z é o "cima" da tela)
-]
-
-# Grid
-cell_plane = pv.Plane(i_size=10, j_size=10, i_resolution=100, j_resolution=100)
-cell_grid = cell_plane.extract_all_edges()
-plotter.add_mesh(cell_grid, show_edges=True, color='#4d4d4d', line_width=1)
-section_plane = pv.Plane(i_size=10, j_size=10, i_resolution=10, j_resolution=10)
-section_grid = section_plane.extract_all_edges()
-plotter.add_mesh(section_grid, show_edges=True, color='#4d4d4d', line_width=3)
-
-# Adiciona qualquer malha ou ator que você queira visualizar
-cube = pv.Cube()
-plotter.add_mesh(cube)
+class IStyles(tp.TypedDict):
+    """Interface para estilos"""
+    theme: str
+    background_color: str
+    grid_size: float
+    cell_size: float
+    cell_width: float
+    cell_color: str
+    section_size: float
+    section_width: float
+    section_color: str
 
 
+class Structure:
+    """Visualização da estrutura"""
+    def __init__(self):
+        self.styles: IStyles = {
+            'theme': 'dark',
+            'background_color': '#1a1a1a',
+            # Grid
+            'grid_size': 10,
 
-# Iniciar o loop de interação
-plotter.show()
+            'cell_size': 0.2,
+            'cell_width': 1,
+            'cell_color': '#4d4d4d',
+
+            'section_size': 1,
+            'section_width': 2,
+            'section_color': '#4d4d4d'
+        }
+
+        self.plotter = pv.Plotter()
+
+
+        # Adiciona qualquer malha ou ator que você queira visualizar
+        cube = pv.Cube((0, 0, 0.5))
+        self.plotter.add_mesh(cube)
+
+    def config(self) -> None:
+        """Faz as configurações iniciais"""
+        # pylint: disable=E1101
+        vtk.vtkObject.GlobalWarningDisplayOff() # Desativar erros
+
+        pv.set_plot_theme(self.styles['theme']) # Tema geral
+
+        self.plotter.background_color = self.styles['background_color'] # Cor de fundo
+        self.plotter.enable_terrain_style(mouse_wheel_zooms=True) # Orbit com o limite no topo
+        self.plotter.camera.view_up = (0, 0, 1) # Eixo "z" para cima
+        self.plotter.add_axes() # Visualização do eixos globais
+
+        self.plotter.camera_position = [
+            (10, -10, 10),  # posição da câmera (x, y, z)
+            (0, 0, 0),     # ponto focal (olhando pro centro da cena)
+            (0, 0, 1)      # view-up (o eixo Z é o "cima" da tela)
+        ]
+
+        # Grid ////////////////////////////////////////////////////////////////////////////////////
+        # Cell ************************************************************************************
+        cell_resolution = int(round(self.styles['grid_size'] / self.styles['cell_size'], 0))
+        cell_plane = pv.Plane(i_size=self.styles['grid_size'], j_size=self.styles['grid_size'],
+                              i_resolution=cell_resolution, j_resolution=cell_resolution)
+        cell_grid = cell_plane.extract_all_edges()
+        self.plotter.add_mesh(cell_grid,
+                              show_edges=True,
+                              color=self.styles['cell_color'],
+                              line_width=self.styles['cell_width'])
+
+        # Section *********************************************************************************
+        section_resolution = int(round(self.styles['grid_size'] / self.styles['section_size'], 0))
+        section_plane1 = pv.Plane((0, 0, 0.001),
+                                 i_size=self.styles['grid_size'], j_size=self.styles['grid_size'],
+                                 i_resolution=section_resolution, j_resolution=section_resolution)
+        section_grid1 = section_plane1.extract_all_edges()
+        self.plotter.add_mesh(section_grid1, show_edges=True,
+                              color=self.styles['section_color'],
+                              line_width=self.styles['section_width'])
+        # Parte inferior para ficar sempre na frente do "cell"
+        section_plane2 = pv.Plane((0, 0, -0.001),
+                                 i_size=self.styles['grid_size'], j_size=self.styles['grid_size'],
+                                 i_resolution=section_resolution, j_resolution=section_resolution)
+        section_grid2 = section_plane2.extract_all_edges()
+        self.plotter.add_mesh(section_grid2, show_edges=True,
+                              color=self.styles['section_color'],
+                              line_width=self.styles['section_width'])
+        # /////////////////////////////////////////////////////////////////////////////////////////
+
+
+    def show(self):
+        """Inicia a visualização"""
+        self.config()
+        self.plotter.show()
+
+a = Structure()
+a.show()
