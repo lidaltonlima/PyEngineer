f97a76527b31b55c1339a6ea733461ed49158cf7
lidaltonlima <lidaltonlima@gmail.com>
2025-08-15 19:09:42 -0400

    add: portico 01 vanilla

---
+++ portico_01_vanilla.py
@@ -0,0 +1,39 @@
+"""Cálculo do pórtico 3"""
+import numpy as np
+
+import pyengineer as pg
+from pyengineer import analysis
+
+
+np.set_printoptions(formatter={'float_kind': '{: .4e}'.format}, linewidth=200)
+
+
+material = pg.Material('steel', 2e11, 7.692308e10, 0.3, 7850)
+# section = pg.Section('w150x13', area=15.74e-4, ix=1.15e-8, iy=596.48e-8, iz=81.76e-8)
+section = pg.Section('w200x52', area=65.69e-4, ix=30.18e-8, iy=5202.99e-8, iz=1783.57e-8)
+
+# Nodes
+nodes: list[pg.Node] = []
+n1 = pg.Node('N1', [0, 0, 0])
+nodes.append(n1)
+n2 = pg.Node('N2', [5, 0, 0])
+nodes.append(n2)
+# Bars
+bars: list[pg.Bar] = []
+b1 = pg.Bar('B1', n1, n2, section, material)
+bars.append(b1)
+
+
+# Loads
+loads: list[pg.Load] = []
+load = pg.Load('L1')
+load.add_node_load('FN1', n2, 10e3, 11e3, 12e3, 10e3, 11e3, 12e3)
+loads.append(load)
+
+# Supports
+support = pg.Support()
+support.add_support(n1, dx=True, dy=True, dz=True, rx=True, ry=True, rz=True)
+
+linear_analysis = analysis.Linear(nodes, bars, loads, support)
+
+print(linear_analysis.get_displacements('N2', 'L1'))
