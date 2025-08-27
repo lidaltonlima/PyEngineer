3726d3fcaa44ff6d7e2def22349db40a5d472be7
lidaltonlima <lidaltonlima@gmail.com>
2025-08-27 06:30:29 -0400

    add: no linear json

--- json_file.py
+++ json_file.py
@@ -5,5 +5,5 @@ from pyengineer.fetch import calculate_json
 
 np.set_printoptions(formatter={'float_kind': '{: .4e}'.format}, linewidth=200)
 
-analysis = calculate_json('./pyengineer/examples/structure_008.json')
+analysis = calculate_json('./pyengineer/examples/no_linear.json')
 print(analysis.get_displacements('N2', 'L1'))
