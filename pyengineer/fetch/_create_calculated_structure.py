e9d9f0910abaec8356345fe34536ba818179320c
lidaltonlima <lidaltonlima@gmail.com>
2025-09-25 17:40:08 -0400

    ref: Calculated structure exit format

--- pyengineer/fetch/_create_calculated_structure.py
+++ pyengineer/fetch/_create_calculated_structure.py
@@ -165,30 +165,55 @@ def create_calculated_structure(path: str, analysis: Linear) -> None:
     structure['loads'] = loads_dict
 
     # Results /////////////////////////////////////////////////////////////////////////////////////
-    results: Dict[str, Dict[str, Dict[str, List[float]]]] = {}
+    results: List[Dict[str, str | list[Dict[str, str | float]]]] = []
 
     # Create dictionary structure for results *****************************************************
-    for load in analysis.loads:
+    for index_load, load in enumerate(analysis.loads):
+        results.append({'load_case': load.name})
         # Get displacements for each node under the current load case -----------------------------
-        displacements: Dict[str, List[float]] = {}
+        displacements: list[Dict[str, str | float]] = []
         for node in analysis.nodes:
             disp_vector = analysis.get_displacements(node.name, load.name)
-            displacements[node.name] = disp_vector.tolist()
-        results[load.name] = {'displacements': displacements}
+            displacements.append({'node': node.name,
+                                         'Dx': disp_vector[0],
+                                         'Dy': disp_vector[1],
+                                         'Dz': disp_vector[2],
+                                         'Rx': disp_vector[3],
+                                         'Ry': disp_vector[4],
+                                         'Rz': disp_vector[5]})
+        results[index_load]['displacements'] = displacements
 
         # Get reactions for each support under the current load case -----------------------------
-        reactions: Dict[str, List[float]] = {}
+        reactions: list[Dict[str, str | float]] = []
         for node in analysis.supports.nodes_support.keys():
             reactions_vector = analysis.get_reactions(node.name, load.name)
-            reactions[node.name] = reactions_vector.tolist()
-        results[load.name]['reactions'] = reactions
+            reactions.append({'node': node.name,
+                              'Fx': reactions_vector[0],
+                              'Fy': reactions_vector[1],
+                              'Fz': reactions_vector[2],
+                              'Mx': reactions_vector[3],
+                              'My': reactions_vector[4],
+                              'Mz': reactions_vector[5]})
+        results[index_load]['reactions'] = reactions
 
         # Get extreme forces for each bar under the current load case -----------------------------
-        extreme_forces: Dict[str, List[float]] = {}
+        extreme_forces: list[Dict[str, str | float]] = []
         for bar in analysis.bars:
             forces_vector = bar.extreme_forces[load.name]
-            extreme_forces[bar.name] = forces_vector.tolist()
-        results[load.name]['extreme_forces'] = extreme_forces
+            extreme_forces.append({'bar': bar.name,
+                                   'Fxi': forces_vector[0],
+                                   'Fyi': forces_vector[1],
+                                   'Fzi': forces_vector[2],
+                                   'Mxi': forces_vector[3],
+                                   'Myi': forces_vector[4],
+                                   'Mzi': forces_vector[5],
+                                   'Fxj': forces_vector[6],
+                                   'Fyj': forces_vector[7],
+                                   'Fzj': forces_vector[8],
+                                   'Mxj': forces_vector[9],
+                                   'Myj': forces_vector[10],
+                                   'Mzj': forces_vector[11]})
+        results[index_load]['extreme_forces'] = extreme_forces
 
     structure['results'] = results
 
--- src/tests/test.py
+++
@@ -1,4 +0,0 @@
-data = 'Dxi; Rxi; Dyi;'
-data_list = [item.strip() for item in data.split(';')]
-
-print('Rxj' in data_list)
